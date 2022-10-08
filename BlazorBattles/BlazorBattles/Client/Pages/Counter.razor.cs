543ce705b5d2f777e41d59f436f87c8a209bbcb1
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2022-10-08 23:52:13 +0100

    starting out

---
+++ BlazorBattles/BlazorBattles/Client/Pages/Counter.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorBattles.Client.Pages
+{
+    public partial class Counter : ComponentBase
+    {
+        private int currentCount = 0;
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
