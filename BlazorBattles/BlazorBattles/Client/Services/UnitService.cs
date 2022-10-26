4dcef2aa9d0556186e9b400419140abd2751ed87
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2022-10-26 16:32:09 +0100

    Login form with basic validation

--- BlazorBattles/BlazorBattles/Client/Services/UnitService.cs
+++ BlazorBattles/BlazorBattles/Client/Services/UnitService.cs
@@ -1,9 +1,15 @@
 ﻿using BlazorBattles.Shared;
+using Blazored.Toast.Services;
 
 namespace BlazorBattles.Client.Services
 {
     public class UnitService : IUnitService
     {
+        private readonly IToastService _toastService;
+        public UnitService(IToastService toastService)
+        {
+            _toastService = toastService;
+        }
         public IList<Unit> Units => new List<Unit> { 
             new Unit { Id = 1, Title = "Knight", Attack = 10, Defence = 10, Cost = 100 },
             new Unit { Id = 2, Title = "Archer", Attack = 15, Defence = 5, Cost = 150 },
@@ -15,6 +21,14 @@ namespace BlazorBattles.Client.Services
         {
             var unit = Units.First(unit => unit.Id == unitId);
             UserUnits.Add(new UserUnit { UnitId = unitId, HitPoints = unit.HitPoints});
+            _toastService.ShowSuccess($"{unit.Title} has been added to your army", "Unit created!");
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is UnitService service &&
+                   EqualityComparer<IList<Unit>>.Default.Equals(Units, service.Units) &&
+                   EqualityComparer<IList<UserUnit>>.Default.Equals(UserUnits, service.UserUnits);
         }
     }
 }
---
+++ BlazorBattles/BlazorBattles/Shared/UserLogin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Please enter a username")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Please enter a password")]
+        [MinLength(8, ErrorMessage = "Your password must be at least 8 characters long")]
+        public string Password { get; set; }
+    }
+}
