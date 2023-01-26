727d37d21448544d035b445b613d116ac5d9fee3
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2023-01-26 14:44:06 +0000

    Create DataContext

---
+++ BlazorBattles/BlazorBattles/Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlazorBattles.Shared;
+
+namespace BlazorBattles.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Unit> Units { get; set; }
+    }
+}
